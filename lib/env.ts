66d264dedd4c88f97c1aacaa0aa3edb51758d8a2
yomjsty <oweychanges@gmail.com>
2025-07-17 15:44:33 +0700

    updated with new zod

--- lib/env.ts
+++ lib/env.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 
 export const env = createEnv({
     server: {
-        DATABASE_URL: z.string().url(),
+        DATABASE_URL: z.url(),
         BETTER_AUTH_SECRET: z.string().min(1),
-        BETTER_AUTH_URL: z.string().url(),
+        BETTER_AUTH_URL: z.url(),
         GITHUB_CLIENT_ID: z.string().min(1),
         GITHUB_CLIENT_SECRET: z.string().min(1),
         RESEND_API_KEY: z.string().min(1),
